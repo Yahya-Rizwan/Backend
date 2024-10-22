79a5adb135f3644f16b0eef01860166bc8f7109a
Mohd Yahya Rizwan <ryahya00@gmail.com>
2024-10-22 11:19:14 +0530

    utils

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try{
-         console.log(process.env.MONGODB_URI);
          const connectionInstance = await mongoose.connect("mongodb+srv://ryahya00:yahya123@youtube.2mpgn.mongodb.net/youtube")
          console.log(`\n MongoDb connected !! DB host ${connectionInstance.connection.host}`);
     }catch(error){
